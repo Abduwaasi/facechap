8714fe4dfcaac653877633a328e0d2312658be88
Abduwaasi <wasiuadekunle73@gmail.com>
2022-06-08 11:34:16 +0100

    mobile drawer component update

--- components/MobileDrawer.js
+++ components/MobileDrawer.js
@@ -4,7 +4,8 @@ import {FaInstagram, FaTwitter,FaLinkedin,FaFacebook,FaBars} from "react-icons/f
 const MobileDrawer = () => {
   return (
     <div className=' w-full h-screen mt-6 bg-white mx-auto flex  flex-col justify-start items-center'>
-      <h2> Social Media</h2>
+      <h2 className='text-transparent  bg-clip-text bg-gradient-to-r 
+        from-[#E6145B]  to-[#7E48A5] text-[2rem] capitalize font-bold'> Social Media</h2>
       <div>
           <a href="https://www.instagram.com/" className='flex  items-center my-6'>
             <FaInstagram size="1.5rem"/>
